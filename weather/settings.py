eb1b4a8d422c37c532090ec02e04581e3d66bd6a
patrick <patricklx@users.noreply.github.com>
2016-07-27 11:20:01 +0100

    include apps

--- weather/settings.py
+++ weather/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework'
+    'rest_framework',
+    'apiv1',
+    'chart',
 ]
 
 MIDDLEWARE_CLASSES = [
